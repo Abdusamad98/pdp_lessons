7e66a9f3f8aaae15c51d29d67e577061e593e8a6
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-03-15 08:47:07 +0500

    first lesson code

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:pdp_lessons/my_logout_button.dart';
-import 'package:pdp_lessons/profile_screen.dart';
 
 main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ProfileScreen(),
-    ),
+    MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold()),
   );
 }
