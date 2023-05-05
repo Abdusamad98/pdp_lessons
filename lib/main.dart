d781a256c8aeacb4e771e73a1a8016057ecf6365
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-05-05 11:08:19 +0500

    first lesson code

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,20 @@ import 'package:flutter/material.dart';
 
 main() {
   runApp(
-    MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold()),
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            "Some Text",
+            style: TextStyle(
+              fontSize: 25,
+              fontFamily: "Inter",
+              fontWeight: FontWeight.w100,
+            ),
+          ),
+        ),
+      ),
+    ),
   );
 }
